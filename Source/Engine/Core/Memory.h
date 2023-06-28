62f9dafa4c19835839b62191a205641c4b2d3321
CalebColby <ccolby@student.neumont.edu>
2023-06-28 14:45:17 -0600

    June28classwork

---
+++ Source/Engine/Core/Memory.h
@@ -0,0 +1,21 @@
+#pragma once
+
+namespace neu
+{
+	struct MemoryAllocation
+	{
+		void* address;
+		size_t size;
+		MemoryAllocation* next;
+	};
+
+	class MemoryTracker
+	{
+	public:
+		//void Add(void* address, size_t size);
+		//void Remove(void* address);
+
+	private:
+
+	};
+}
--- Source/Game/Game/main.cpp
+++ Source/Game/Game/main.cpp
@@ -1,14 +1,33 @@
 #include "../../Engine/Core/Random.h"
 #include "../../Engine/Core/FileIO.h"
+#include "../../Engine/Core/Memory.h"
 #include <iostream>
 
 
 using namespace std;
 
+
 int main() 
 {
+	int* p = new int;
+	delete p;
+
+
+
+
+
+
+	cout << neu::getFilePath() << endl;
+	neu::setFilePath("Assets");
 	cout << neu::getFilePath() << endl;
 
+	size_t size;
+	neu::getFileSize("Game.txt", size);
+	cout << size << endl;
+
+	std::string buffer;
+	neu::readFile("Game.txt", buffer);
+	cout << buffer << endl;
 
 	neu::seedRandom((unsigned int)time(nullptr));
 	for (size_t i = 0; i < 10; i++)
